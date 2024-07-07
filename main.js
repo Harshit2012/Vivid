aa09e0b9376098ed9f5e9d2db41c396f7eee6bcc
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2024-07-07 12:50:02 +0530

    Bootstrap tooltip added

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let selectedElement = null;
 const createdElements = [];
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 function addComponent(type) {
     let element;
