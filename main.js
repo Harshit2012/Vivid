5fd58fde6a3614f7e494b68418b773372667497c
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2024-07-07 13:02:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let selectedElement = null;
 const createdElements = [];
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 
 function addComponent(type) {
     let element;
